f27e45a2572ac707bb8c57744d755890bac4d135
Andrey Moujikov <a@moujikov.email>
2026-03-22 10:34:10 +0300

    setup script first version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# 6k2_services
-Services deployment and orchestration 
+# New VPS setup steps:
+1. SSH as root or `sudo` all commands below
+2. Update and upgrade packages:
+   ```
+   apt update && apt upgrade -y
+   ```
+3. Install git:
+   ```
+   apt install -y git
+   ```
+4. Clone the repository:
+   ```
+   git clone https://github.com/moujikov/6k2_services /opt/services
+   ```
+5. Run the setup script:
+   ```
+   /opt/services/setup.sh
+   ```
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+apt update
+apt install -y --no-install-recommends \
+  ca-certificates curl gnupg
+
+if [ ! -d '/etc/apt/keyrings' ]; then
+  install -m 0755 -d '/etc/apt/keyrings'
+fi
+
+REPO='https://download.docker.com/linux/ubuntu'
+DOCKER_GPG='/etc/apt/keyrings/docker.gpg'
+DOCKER_SOURCES='/etc/apt/sources.list.d/docker.list'
+ARCH=$(dpkg --print-architecture)
+OS_RELEASE=$(. /etc/os-release && echo $VERSION_CODENAME)
+
+if [ ! -f $DOCKER_GPG ]; then
+  curl -fsSL $REPO/gpg | gpg --dearmor -o $DOCKER_GPG
+  chmod a+r $DOCKER_GPG
+fi
+
+if [ ! -f $DOCKER_SOURCES ]; then
+  > $DOCKER_SOURCES echo "deb [arch=$ARCH signed-by=$DOCKER_GPG] $REPO $OS_RELEASE stable"
+fi
+
+apt update
+
+if [ ! $(apt-cache policy docker-ce | grep -q "$REPO") ]; then
+  echo "ERROR: Docker repository setup failed."
+  exit 1
+fi
+
+apt install -y --no-install-recommends \
+  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
