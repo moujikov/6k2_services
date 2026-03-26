5d6911766903a28b5e4a7bf9dc413865aea77137
Andrey Moujikov <a@moujikov.email>
2026-03-26 10:18:26 +0300

    generating secrets

--- setup.sh
+++ setup.sh
@@ -2,11 +2,19 @@
 
 set -eu -o pipefail
 
+if [ $EUID -ne 0 ]; then
+   echo "This script is not running as root. Please use sudo."
+   exit 1
+fi
+
 REPO='https://download.docker.com/linux/ubuntu'
 DOCKER_GPG='/etc/apt/keyrings/docker.gpg'
 DOCKER_SOURCES='/etc/apt/sources.list.d/docker.list'
 ARCH=$(dpkg --print-architecture)
 OS_RELEASE=$(. /etc/os-release && echo $VERSION_CODENAME)
+
+
+### INSTALLING PACKAGES
 
 apt-get update
 apt-get install -y --no-install-recommends \
@@ -36,11 +44,38 @@ apt-get install -y --no-install-recommends \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
+### SETTING UP ENVIRONMENT
+
+ensure_secret_file() {  
+  [ -f "$1" ] || install -m 0600 /dev/null "$1"
+}
+
+set_secret() {
+  install -m 0600 /dev/null "$1"
+  echo "$2" >> "$1"
+}
+
+generate_secret() {
+  local file="$1"
+  local length=${2:-16}
+  local prefix="${3:-}"
+  
+  if [ -f "$file" ]; then
+    echo "File '$file' already exists. Delete it to regenerate. Skipping..."; echo
+  else
+    install -m 0600 /dev/null "$file"
+    printf "$prefix" >> "$file"
+    cat /dev/random | LC_ALL=C tr -dc 'A-Za-z0-9' | head -c $length >> "$file"
+    echo "Generated secret in '$file'"; echo
+  fi
+}
+
+
 install -m 0755 -d '/usr/local/share/traefik'
 install -m 0700 -d '/usr/local/share/traefik/auth'
-install -m 0600 /dev/null '/usr/local/share/traefik/auth/admins'
-install -m 0600 /dev/null '/usr/local/share/traefik/auth/users'
-install -m 0600 /dev/null '/usr/local/share/traefik/auth/timeweb_auth_token'
+
+install -m 0755 -d '/usr/local/share/database'
+install -m 0700 -d '/usr/local/share/database/auth'
 
 
 DOCKER_USER='moujikov'
@@ -52,14 +87,19 @@ if [ -n "$DOCKER_PAT" ]; then
   unset DOCKER_PAT
 fi
 
-
 read -s -p "Provide Timeweb Clound auth token (empty to skip): " TIMEWEB_AUTH_TOKEN
 echo
 
 if [ -n "$TIMEWEB_AUTH_TOKEN" ]; then
-  echo "$TIMEWEB_AUTH_TOKEN" > '/usr/local/share/traefik/auth/timeweb_auth_token'
+  set_secret '/usr/local/share/traefik/auth/timeweb_auth_token' "$TIMEWEB_AUTH_TOKEN"
   unset TIMEWEB_AUTH_TOKEN
 fi
+
+generate_secret '/usr/local/share/database/auth/db_password_postgres' 32 __postgres_  
+generate_secret '/usr/local/share/database/auth/db_password_authelia' 32 __authelia_
+
+ensure_secret_file '/usr/local/share/traefik/auth/admins'
+ensure_secret_file '/usr/local/share/traefik/auth/users'
 
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
