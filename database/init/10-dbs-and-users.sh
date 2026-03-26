0ecf7d80e47d46cf571260d112a19b1c2216ac6c
Andrey Moujikov <a@moujikov.email>
2026-03-26 10:55:22 +0300

    database added

---
+++ database/init/10-dbs-and-users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+create_user() {
+	local DATABASE=$1
+	local USER=$2
+	local PASSWORD="$(< /run/secrets/db_password_$USER)"
+
+	psql -v ON_ERROR_STOP=1 <<-EOSQL
+		CREATE USER $USER WITH PASSWORD '$PASSWORD';
+		CREATE DATABASE $DATABASE WITH OWNER $USER;
+	EOSQL
+}
+
+create_db_and_user authelia authelia
